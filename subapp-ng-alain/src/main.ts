32d3c7fa5060c70fd77427673bdc8e600fe1c783
chenwei <chenw@shuwang.info>
2020-07-21 21:46:33 +0800

    子应用Angular改造为支持qiankun(single-spa)

--- subapp-ng-alain/src/main.ts
+++ subapp-ng-alain/src/main.ts
@@ -1,5 +1,6 @@
-import { enableProdMode, ViewEncapsulation } from '@angular/core';
+import { enableProdMode, NgZone, ViewEncapsulation } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Router } from '@angular/router';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -9,31 +10,45 @@ preloaderFinished();
 
 import { hmrBootstrap } from './hmr';
 
+import { getSingleSpaExtraProviders, singleSpaAngular } from 'single-spa-angular';
+import { singleSpaPropsSubject } from './single-spa/single-spa-props';
+
 if (environment.production) {
   enableProdMode();
 }
 
-const bootstrap = () => {
-  return platformBrowserDynamic()
-    .bootstrapModule(AppModule, {
+if (!(window as any).__POWERED_BY_QIANKUN__) {
+  const bootstrap = async () => {
+    const res = await platformBrowserDynamic().bootstrapModule(AppModule, {
       defaultEncapsulation: ViewEncapsulation.Emulated,
-    })
-    .then(res => {
-      if ((window as any).appBootstrap) {
-        (window as any).appBootstrap();
-      }
-      return res;
     });
-};
+    if ((window as any).appBootstrap) {
+      (window as any).appBootstrap();
+    }
+    return res;
+  };
 
-if (environment.hmr) {
-  // tslint:disable-next-line: no-string-literal
-  if (module['hot']) {
-    hmrBootstrap(module, bootstrap);
+  if (environment.hmr) {
+    // tslint:disable-next-line: no-string-literal
+    if (module['hot']) {
+      hmrBootstrap(module, bootstrap);
+    } else {
+      console.error('HMR is not enabled for webpack-dev-server!');
+      console.log('Are you using the --hmr flag for ng serve?');
+    }
   } else {
-    console.error('HMR is not enabled for webpack-dev-server!');
-    console.log('Are you using the --hmr flag for ng serve?');
+    bootstrap();
   }
-} else {
-  bootstrap();
 }
+
+const { bootstrap, mount, unmount } = singleSpaAngular({
+  bootstrapFunction: (singleSpaProps) => {
+    singleSpaPropsSubject.next(singleSpaProps);
+    return platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule);
+  },
+  template: '<app-root />',
+  Router,
+  NgZone,
+});
+
+export { bootstrap, mount, unmount };
